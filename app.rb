96ccc67f5a20395c0be50b23c3217f868befdd40
Harley McDowell <hmcdowell95@gmail.com>
2020-04-25 06:49:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
   
-  post "/" do
+  post "/piglatinize" do
     the_text = params[:user_phrase]
     @pig_text = PigLatinizer.new.piglatinize(the_text)
     erb :results
