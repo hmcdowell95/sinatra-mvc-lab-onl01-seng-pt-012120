dcf527feffaeb853a4822417228fe9228dbabb99
Harley McDowell <hmcdowell95@gmail.com>
2020-04-25 06:12:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
-
+require_relative 'models/piglatinizer.rb'
 class App < Sinatra::Base
+  
+  get "/" do 
+    erb :user_input
+  end
+  
+  post "/" do
+    the_text = params[:user_text]
+    @pig_text = PigLatinizer.new.piglatinize(the_text)
+    erb :results
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,34 @@
+class PigLatinizer
+  
+  def piglatinize(w)
+    text = w.downcase.split(" ")
+    i = []
+    text.each do |x|
+      if x.chars.first.scan(/[aeiou]/).count == 1
+        a = [x, 'way'].join
+        i << a
+      elsif x.scan(/[aeiou]/).count == 0
+        i << x
+      else
+        z = x.chars
+        y = z.first
+        a = []
+        begin
+          a << y
+          z.shift
+        end until y.scan(/[aeiou]/).count == 1
+        a << "ay"
+        b = a.join
+        z << b
+        z.join
+        i << z
+      end
+    end
+  end
+        
+        
+  
+end
+    
+  
+  
